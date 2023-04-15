3dde7bf1e9dbd07152674f8ac339a421ca4876fc
DianaMMartins <dian_marq_mart@hotmail.com>
2023-04-15 14:48:15 +0100

    trying to figure out why fe is not getting right info

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -3,6 +3,7 @@ const { selectCategories } = require("../models/categoriesModels");
 exports.getCategories = (request, response, next) => {
   selectCategories()
     .then((categories) => {
+      console.log(categories, 'back end get categories');
       response.status(200).send(categories);
     })
     .catch((error) => {
